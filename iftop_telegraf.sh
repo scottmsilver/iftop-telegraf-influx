30a24a6e408f675b0358f4709d69bef5b0017c2a
scottmsilver <scottmsilver@users.noreply.github.com>
2020-10-03 08:50:27 -0700

    Parameterize which interface to monitor.

--- iftop_telegraf.sh
+++ iftop_telegraf.sh
@@ -5,5 +5,8 @@ SCRIPT=$(readlink -f "${0}")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
+# Update this interface to the interface you wish to monitor.
+# Typically this is the LAN interface (the one that is your private network at home)
+IFACE=igb1
 
-/usr/local/sbin/iftop -nNb -i igb1 -s 1 -o 2s -t -L 100 2>/dev/null | awk -f $SCRIPTPATH/iftop_parse.awk | $SCRIPTPATH/iftop_telegraf.py
+/usr/local/sbin/iftop -nNb -i $IFACE -s 1 -o 2s -t -L 100 2>/dev/null | awk -f $SCRIPTPATH/iftop_parse.awk | $SCRIPTPATH/iftop_telegraf.py
