ed74ddc32db05f19758058b9540ad77fa675f3c6
scottmsilver <scottmsilver@users.noreply.github.com>
2020-10-03 08:51:58 -0700

    More comments.

--- iftop_telegraf.sh
+++ iftop_telegraf.sh
@@ -9,4 +9,6 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 # Typically this is the LAN interface (the one that is your private network at home)
 IFACE=igb1
 
+# Get a list of talkers by sampling for 2 seconds, up to 100 streams, and then dump themp out
+# to an awk script that pull out all but the hosts, then parse them in a simple python script that outputs the data in telegraf line format.
 /usr/local/sbin/iftop -nNb -i $IFACE -s 1 -o 2s -t -L 100 2>/dev/null | awk -f $SCRIPTPATH/iftop_parse.awk | $SCRIPTPATH/iftop_telegraf.py
